f1a9778be186fb0223567e978c2fd7cff4abc0bc
Eze <eze.largaespada@gmail.com>
2020-06-08 21:17:54 -0400

    mongo

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"|| "mongodb://ezequiel:Hialeah305@ds125486.mlab.com:25486/heroku_rz030z7j";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
 
 // routes
 app.use(require("./routes/api.js"));
